5b6bbe87d8ed94814480e833169fb86df1a2bc9d
jiancheng <seesawjack14@gmail.com>
2022-04-27 23:05:51 +0800

    0427-test

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,8 +2,27 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name:"",
+      confirmName:""
     };
   },
+  methods:{
+    confirmNameInput(){
+      this.confirmName = this.name
+    },
+    add(num){
+      this.counter= this.counter+num
+    },
+    menus(num){
+      this.counter = this.counter-num
+    },
+    addName(event,lastName){
+    this.name = event.target.value+" "+lastName
+    },
+    send(){
+      alert("you send")
+    }
+  }
 });
 
 app.mount('#events');
