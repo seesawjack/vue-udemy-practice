bddf8427fbc784354b4f29df548676fafdbce813
jiancheng <seesawjack14@gmail.com>
2022-05-18 23:12:13 +0800

    vue-component-hk-02

---
+++ cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="box">
+        <strong>My name is:{{name}}</strong>
+        <br>
+        <strong>{{age}} Years</strong>
+    </div>
+</template>
+
+<script>
+export default {
+   props:{
+       name:{
+           type:String,
+           required:true
+       },
+       age:{
+           type:Number,
+            required:true
+       }
+   }
+}
+</script>
---
+++ cmp-communication-assignment-problem/src/components/UserData.vue
@@ -0,0 +1,32 @@
+<template>
+    <form @submit.prevent="submitData">
+        <div>
+            <input type="text" v-model="enterName" />
+        </div>
+        <div>
+            <input type="text" v-model="enterAge" />
+        </div>
+        <button>Submit</button>
+    </form>
+</template>
+<script>
+export default {
+    emits:['add-contact'],
+    data(){
+        return {
+            enterName:'',
+            enterAge:''
+        }
+    },
+    methods:{
+        submitData(){
+            this.$emit(
+                'add-contact',
+                this.enterName,
+                this.enterAge
+            )
+        }
+    }
+    
+}
+</script>
