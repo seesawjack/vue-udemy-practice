2078a98655377df46646161430cb10d7cbcf05d9
jiancheng <seesawjack14@gmail.com>
2022-05-29 23:22:44 +0800

    git-project-0529

--- prj-cmp-01-starting-setup/src/App.vue
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -1,15 +1,13 @@
 <template>
   <the-header title="Remember Me"></the-header>
-   <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
-import StoredResources from './components/learning-resource/StoredResources.vue';
+
 import TheHeader from './components/layouts/TheHeader.vue'
 
 export default {
     components:{
-        StoredResources,
         TheHeader
     },
     data(){
---
+++ prj-cmp-01-starting-setup/src/components/learning-resource/TheResource.vue
@@ -0,0 +1,28 @@
+<template>
+    <base-card>
+        <base-button @click="setSelectedTab('stored-resources')">Stored Resources</base-button>
+        <base-button @click="setSelectedTab('add-resource')">Add Resource</base-button>
+    </base-card>
+    <component :is="selectedTab"></component>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue'
+ export default{
+     components:{
+         StoredResources,
+         AddResource
+     },
+     data(){
+         return{
+             selectedTab:'store-resources'
+         }
+     },
+     methods:{
+         setSelectedTab(tab){
+             this.selectedTab = tab;
+         }
+     }
+ }
+</script>
