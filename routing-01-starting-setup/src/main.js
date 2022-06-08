3bf00c623f32c8593def29f4ef223e12e90941d3
jiancheng <seesawjack14@gmail.com>
2022-06-08 23:07:51 +0800

    vue-router-0608

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -3,13 +3,15 @@ import { createRouter,createWebHistory } from 'vue-router';
 
 import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
-import UsersList from './components/users/UsersList.vue'
+import UsersList from './components/users/UsersList.vue';
+import TeamMembers from './components/teams/TeamMembers.vue'
 
 const router = createRouter({
     history:createWebHistory(),
     routes:[
         {path:'/teams',component:TeamsList}, //our-domain.com/teams => TeamsList
         {path:'/users',component:UsersList}, //our-domain.com/users => UsersList
+        {path:'/teams/:teamId',component:TeamMembers},
     ],
     linkActiveClass:'active'
 })
