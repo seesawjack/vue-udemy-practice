2fcaaecc7612293013499078872403a3effb9fe3
jiancheng <seesawjack14@gmail.com>
2022-06-14 22:08:33 +0800

    Using Nested Routes

--- routing-01-starting-setup/src/main.js
+++ routing-01-starting-setup/src/main.js
@@ -12,9 +12,14 @@ const router = createRouter({
     routes:[
         {path:'/',redirect:'/teams'},  //當網址輸入'/'時，回重新導向到'/teams'
         // {path:'/teams',component:TeamsList,alias:'/'}, //當在網址輸入'/' 時，網頁不會重新導向到新的網址，反而是替換成TeamsList這個component
-        {path:'/teams',component:TeamsList}, //our-domain.com/teams => TeamsList
+        {
+            path:'/teams',
+            component:TeamsList,
+            children:[
+                {path:':teamId',component:TeamMembers,props:true},
+            ]
+        }, //our-domain.com/teams => TeamsList
         {path:'/users',component:UsersList}, //our-domain.com/users => UsersList
-        {path:'/teams/:teamId',component:TeamMembers,props:true},
         {path:'/:notFound(.*)',component:NotFound} //若使用者在網址列輸入無效的內容，則可以透過'/:名稱自訂(.*)'重定向到指定的component
     ],
     linkActiveClass:'active'
