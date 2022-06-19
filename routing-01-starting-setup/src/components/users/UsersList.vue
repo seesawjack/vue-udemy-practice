5436b35d09ab6e8d7f228cd98abd0d80fc2034b5
jiancheng <seesawjack14@gmail.com>
2022-06-19 14:40:19 +0800

    Beyond Entering: Route Leave Guards

--- routing-01-starting-setup/src/components/users/UsersList.vue
+++ routing-01-starting-setup/src/components/users/UsersList.vue
@@ -1,5 +1,6 @@
 <template>
 <button @click="confirmInput">Confirm</button>
+<button @click="saveChanges">Save Change</button>
   <ul>
     <user-item v-for="user in users" :key="user.id" :name="user.fullName" :role="user.role"></user-item>
   </ul>
@@ -13,15 +14,33 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  data(){
+    return{
+      changeSaved:false
+    }
+  },
   methods:{
     confirmInput(){
       this.$router.push('/teams')
+    },
+    saveChanges(){
+      this.changeSaved = true;
     }
   },
   beforeRouteEnter(to,from,next){
     console.log('UsersList Cmp beforeRouteEnter');
     console.log(to,from);
     next();
+  },
+  beforeRouteLeave(to,from,next){
+    console.log('Userslist cmp beforeRouterLeave')
+    console.log(to,from);
+    if(this.changeSaved){
+      next();
+    }else{
+      const userWantsLeave =  confirm('您確定要離開頁面嗎?')
+      next(userWantsLeave);
+    }
   }
 };
 </script>
