158f10d6466c94c6c659c25608d9a75111f9c01a
jiancheng <seesawjack14@gmail.com>
2022-06-30 23:02:17 +0800

    0630-Registering Routes

---
+++ main-prj-01-starting-setup/src/router.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+    history:createWebHistory(),
+    routes: [
+        {path: '/', redirect: '/coaches'},
+        {path: '/coaches', component: null},
+        {path: '/coaches/:id', component: null , children:[
+            {path: '/contact', components:null}  //coaches/id/contact
+        ]},
+        {path: '/register', component:null},
+        {path: '/request', component:null},
+        {path: '/:notFound(.*)',component:null}
+
+    ],
+});
+
+export default router;
