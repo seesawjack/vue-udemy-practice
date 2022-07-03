c1e0e4f203b1c8abcda75fafe5963afe1f1cae49
jiancheng <seesawjack14@gmail.com>
2022-07-03 22:51:58 +0800

    Adding Route Page Components

--- main-prj-01-starting-setup/src/router.js
+++ main-prj-01-starting-setup/src/router.js
@@ -1,16 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import CoachDetail from './pages/coaches/CoachDetail.vue';
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachRegistration from './pages/coaches/CoachRegistration.vue';
+
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue'
+
 const router = createRouter({
     history:createWebHistory(),
     routes: [
         {path: '/', redirect: '/coaches'},
-        {path: '/coaches', component: null},
-        {path: '/coaches/:id', component: null , children:[
-            {path: '/contact', components:null}  //coaches/id/contact
-        ]},
-        {path: '/register', component:null},
-        {path: '/request', component:null},
-        {path: '/:notFound(.*)',component:null}
+        {path: '/coaches', component: CoachesList},
+        {
+            path: '/coaches/:id', 
+            component: CoachDetail , 
+            children:[
+                {path: '/contact', components:ContactCoach}  //coaches/id/contact
+            ]
+        },
+        {path: '/register', component:CoachRegistration},
+        {path: '/request', component:RequestsReceived},
+        {path: '/:notFound(.*)',component:NotFound}
 
     ],
 });
