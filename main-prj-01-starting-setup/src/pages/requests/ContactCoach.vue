52e09d30cc98c34d98bbce8fb08df6a9f06a8b58
jiancheng <seesawjack14@gmail.com>
2022-09-07 21:53:33 +0800

    247. Working on the Contact Form

--- main-prj-01-starting-setup/src/pages/requests/ContactCoach.vue
+++ main-prj-01-starting-setup/src/pages/requests/ContactCoach.vue
@@ -1,3 +1,81 @@
 <template>
-    CONTACT A COACH
+    <form @submit.prevent="submitForm">
+        <div class="form-control">
+            <label for="email">你的信箱</label>
+            <input type="email" id="email" v-model.trim="email"/>
+        </div>
+        <div class="form-control">
+            <label for="message">你的留言</label>
+            <textarea rows="5" id="message" v-model.trim="message"></textarea>
+        </div>
+        <p class="errors" v-if="!formIsValid">請輸入有效電子信箱及留言</p>
+        <div class="actions">
+            <base-button>傳送訊息</base-button>
+        </div>
+    </form>
 </template>
+
+<script>
+export default {
+    data(){
+        return{
+            email:'',
+            message:'',
+            formIsValid:true
+        }
+    },
+    methods:{
+        submitForm(){
+            this.formIsValid = true;
+            if(this.email === '' || !this.email.includes('@') || this.message === ''){
+                this.formIsValid = false;
+                return;
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+form {
+  margin: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+
+.errors {
+  font-weight: bold;
+  color: red;
+}
+
+.actions {
+  text-align: center;
+}
+</style>
