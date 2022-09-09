be2e97e2b75ab5840f3d4c0f0de6870c8a69c75b
jiancheng <seesawjack14@gmail.com>
2022-09-09 23:25:12 +0800

    252. Fetching Coach Data (GET Http Request)

--- main-prj-01-starting-setup/src/store/modules/coaches/actions.js
+++ main-prj-01-starting-setup/src/store/modules/coaches/actions.js
@@ -21,5 +21,27 @@ export default {
             ...coachData,
             Id:userId
         })
+    },
+    async loadCoaches(context){
+        const response = await fetch(`https://vue-http-5cc0d-default-rtdb.firebaseio.com/coaches/.json`);
+        const responseData = await response.json();
+
+        if(!response.ok){
+            //error
+        }
+
+        const coaches = [];
+        for(const key in responseData){
+            const coach = {
+                id:key,
+                firstName:responseData[key].firstName,
+                lastName:responseData[key].lastName,
+                description:responseData[key].description,
+                hourlyRate:responseData[key].hourlyRate,
+                areas:responseData[key].areas
+            }
+            coaches.push(coach)
+        }
+        context.commit('setCoaches',coaches)
     }
 }
