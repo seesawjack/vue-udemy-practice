16a235abb8634f025bb61984120a9c12e35e1e87
jiancheng <seesawjack14@gmail.com>
2022-09-11 12:16:20 +0800

    257. Adding Route Transitions

--- main-prj-01-starting-setup/src/pages/requests/RequestsReceived.vue
+++ main-prj-01-starting-setup/src/pages/requests/RequestsReceived.vue
@@ -1,25 +1,27 @@
 <template>
-    <base-dialog :show="!!error" title="錯誤發生" @close="handleError">
-        <p>{{ error }}</p>
-    </base-dialog>
-    <section>
-        <base-card>
-            <header>
-                <h2>Requests Received</h2>
-            </header>
-            <base-spinner v-if="isLoading">
-            </base-spinner>
-            <ul v-else-if="hasRequests && !isLoading">
-                <request-item 
-                v-for="req in receivedRequests" 
-                :key="req.id"
-                :email="req.userEmail"
-                :message="req.message"
-                ></request-item>
-            </ul>
-            <h3 v-else>尚未有任何消息</h3>
-        </base-card>
-    </section>
+    <div>
+        <base-dialog :show="!!error" title="錯誤發生" @close="handleError">
+            <p>{{ error }}</p>
+        </base-dialog>
+        <section>
+            <base-card>
+                <header>
+                    <h2>Requests Received</h2>
+                </header>
+                <base-spinner v-if="isLoading">
+                </base-spinner>
+                <ul v-else-if="hasRequests && !isLoading">
+                    <request-item 
+                    v-for="req in receivedRequests" 
+                    :key="req.id"
+                    :email="req.userEmail"
+                    :message="req.message"
+                    ></request-item>
+                </ul>
+                <h3 v-else>尚未有任何消息</h3>
+            </base-card>
+        </section>
+    </div>  
 </template>
 
 <script>
