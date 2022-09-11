c0bea9b9294a6a456710e6759cdc0d289112d4fc
jiancheng <seesawjack14@gmail.com>
2022-09-11 12:19:53 +0800

    258. The "Not Found" Page & Summary

---
+++ main-prj-01-starting-setup/src/pages/NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+    <section>
+        <base-card>
+            <h2>找不到該頁面</h2>
+            <p>
+                找不到該頁，也許可以看看我們的<router-link to="/coaches">教練</router-link>
+            </p>
+        </base-card>
+    </section>
+</template>
