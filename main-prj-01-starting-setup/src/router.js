9b325a1e120e837b6113fd6885047f6f900b21c7
jiancheng <seesawjack14@gmail.com>
2022-09-22 22:46:02 +0800

    263. Adding an Authentication Page (Login & Signup)

--- main-prj-01-starting-setup/src/router.js
+++ main-prj-01-starting-setup/src/router.js
@@ -6,7 +6,8 @@ import CoachRegistration from './pages/coaches/CoachRegistration.vue';
 
 import ContactCoach from './pages/requests/ContactCoach.vue';
 import RequestsReceived from './pages/requests/RequestsReceived.vue';
-import NotFound from './pages/NotFound.vue'
+import NotFound from './pages/NotFound.vue';
+import UserAuth from './pages/auth/UserAuth.vue'
 
 const router = createRouter({
     history:createWebHistory(),
@@ -23,6 +24,7 @@ const router = createRouter({
         },
         {path: '/register', component:CoachRegistration},
         {path: '/requests', component:RequestsReceived},
+        {path: '/auth', component:UserAuth},
         {path: '/:notFound(.*)',component:NotFound}
 
     ],
