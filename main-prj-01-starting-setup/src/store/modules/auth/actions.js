d9d8aca258e753419fe4cddccd9233729d0dd8a6
jiancheng <seesawjack14@gmail.com>
2022-09-25 16:08:10 +0800

    272. Adding "Auto Login"

--- main-prj-01-starting-setup/src/store/modules/auth/actions.js
+++ main-prj-01-starting-setup/src/store/modules/auth/actions.js
@@ -1,6 +1,23 @@
 export default {
     async login(context,payload){
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDR5jae5927AxL92A7Z0jewL4CQWlgtfz8',{
+       return context.dispatch('auth',{
+        ...payload,
+        mode:'login'
+       })
+    },
+    async signup(context,payload){
+        return context.dispatch('auth',{
+            ...payload,
+            mode:'signup'
+           })
+    },
+    async auth(context,payload){
+        const mode = payload.mode;
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDR5jae5927AxL92A7Z0jewL4CQWlgtfz8';
+        if(mode === 'signup'){
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDR5jae5927AxL92A7Z0jewL4CQWlgtfz8'
+        }
+        const response = await fetch(url,{
             method:'POST',
             body:JSON.stringify({
                 email:payload.email,
@@ -13,7 +30,8 @@ export default {
             const error =new Error(responseData.message || '認證失敗...');
             throw error
         }
-        console.log(responseData)
+        localStorage.setItem('token',responseData.idToken)
+        localStorage.setItem('userId',responseData.localId)
 
         context.commit('setUser',{
             token:responseData.idToken,
@@ -21,27 +39,17 @@ export default {
             tokenExpiration:responseData.expiresIn
         })
     },
-    async signup(context,payload){
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDR5jae5927AxL92A7Z0jewL4CQWlgtfz8',{
-            method:'POST',
-            body:JSON.stringify({
-                email:payload.email,
-                password:payload.password,
-                returnSecureToken:true
-            })
-        })
-        const responseData = await response.json();
-        if(!response.ok){
-            const error =new Error(responseData.message || '認證失敗...');
-            throw error
-        }
-        console.log(responseData)
+    tryLogin(context){
+        const token = localStorage.getItem('token');
+        const userId = localStorage.getItem('userId');
 
-        context.commit('setUser',{
-            token:responseData.idToken,
-            userId:responseData.localId,
-            tokenExpiration:responseData.expiresIn
-        })
+        if(token && userId){
+            context.commit('setUser',{
+                token:token,
+                userId:userId,
+                tokenExpiration:null
+            })
+        }
     },
     logout(context){
         context.commit('setUser',{
